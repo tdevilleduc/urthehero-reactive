23be2febe709bea10b26809126c2b5f0bedd7817
Thomas Deville-Duc <devilleduc.thomas@gmail.com>
2020-03-28 11:49:50 +0100

    Initial commit

---
+++ src/main/java/com/tdevilleduc/urthehero/reactive/UrtheheroReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.tdevilleduc.urthehero.reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrtheheroReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrtheheroReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdevilleduc/urthehero/reactive/UrtheheroReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdevilleduc.urthehero.reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrtheheroReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
